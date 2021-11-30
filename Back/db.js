e9cc4fb48f570973ff720ba72c4e55e95f53b426
Cristian Bello <cristian.bello6@gmail.com>
2021-11-29 20:37:18 -0500

    Initial commit

--- Back/db.js
+++ Back/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const db = {
   connect: function () {
-    mongoose.connect("mongodb+srv://cbello95:Catalina95*@cluster0.cxeul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    mongoose.connect("mongodb+srv://cbello95:Catalina95*@cluster0.cxeul.mongodb.net/test", {
     //mongoose.connect("mongodb+srv://cbello95:Catalina95*@cluster0.6uqoh.mongodb.net/test",{  
     useNewUrlParser: true,
     });
