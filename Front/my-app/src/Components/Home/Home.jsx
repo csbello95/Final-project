744b880675b6db56093e7c6382c36d7b48a1a924
Cristian Bello <cristian.bello6@gmail.com>
2021-11-30 20:04:39 -0500

    add rentals component and adjust home

--- Front/my-app/src/Components/Home/Home.jsx
+++ Front/my-app/src/Components/Home/Home.jsx
@@ -1,20 +1,75 @@
 import React, { useEffect, useState, useRef } from 'react'
 import circle from "../../assets/circle.png"
 import { Card, Button, Form, Col, Row } from "react-bootstrap";
+import Pay from '../Pay';
 import axios from "axios";
+import { API_URL } from '../../Constants';
 
 
 import './Home.scss'
-import { API_URL } from '../../Constants';
-//import { Cars } from '../../../../../Back/models';
+
 const Home = () => {
     const [cars, setCars] = useState([]);
+    const [price, setPrice] = useState(0);
     const [selectedCar, setSelectedCar] = useState(cars[0]);
+    const [rentalDates, setRentalDates] = useState({
+        from: 0,
+        to: 0,
+    });
+    const [payModal, setPayModal] = useState({
+        showModal: false,
+        rentalData: undefined,
+    })
+
     const scrollRef = useRef(null);
+
+    const getTotalRental = () => {
+        const { from, to } = rentalDates;
+        const days = to - from + 1;
+        const total = days * selectedCar.rental_value;
+        if (total < 0) {
+            setPrice(0);
+        } else {
+            setPrice(total);
+        }
+    }
+
     const handleSelect = (i) => {
         setSelectedCar(cars[i]);
         scrollRef.current.scrollIntoView();
     }
+
+    const handleDates = (tag, event) => {
+        setRentalDates((prevState) => ({
+            ...prevState,
+            [tag]: Date.parse(event.target.value) / (1000 * 60 * 60 * 24),
+        }));
+    };
+
+    const handlePay = (event) => {
+        event.preventDefault();
+        const [email, name, phone, from, to] = event.target;
+        const rentalData = {
+            email: email.value,
+            name: name.value,
+            phone: phone.value,
+            from: from.value,
+            to: to.value,
+            total: price,
+            id_car: selectedCar._id,
+        };
+        console.log(rentalData);
+        setPayModal({ showModal: true, rentalData })
+        event.target.reset();
+    };
+
+    useEffect(() => {
+        if (selectedCar && rentalDates) {
+            getTotalRental();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedCar, rentalDates])
+
 
     useEffect(() => {
         const loadCars = async () => {
@@ -41,7 +96,7 @@ const Home = () => {
                 <h1>Alquila facilmente nuestros autos</h1>
                 <section className="cars-container">
                     {cars.map(({ car_brand, car_model, number_doors, number_bags, image, scale, rental_value }, i) => (
-                        <Card style={{ width: '22rem' }}>
+                        <Card style={{ width: '22rem' }} key={i}>
                             <Card.Img variant="top" src={image} />
                             <Card.Body>
                                 <Card.Title>{`${car_brand} ${car_model}`}</Card.Title>
@@ -50,7 +105,9 @@ const Home = () => {
                                     <li>N° Maletas: {number_bags}</li>
                                     <li>Gama: {scale}</li>
                                 </ul>
-                                <h5>${rental_value}</h5>
+                                <h5>${rental_value.toLocaleString(navigator.language, {
+                                    minimumFractionDigits: 0,
+                                })}</h5>
                                 <Button onClick={() => { handleSelect(i) }}>Alquilar</Button>
                             </Card.Body>
                         </Card>
@@ -81,11 +138,11 @@ const Home = () => {
                                     <li>N° Maletas:{selectedCar?.number_bags} </li>
                                     <li>Gama:{selectedCar?.scale}</li>
                                 </ul>
-                                <h5>${selectedCar?.rental_value}</h5>
+                                <h5>${selectedCar?.rental_value.toLocaleString(navigator.language, { minimumFractionDigits: 0, })}</h5>
                                 <Button>Alquilar</Button>
                             </Card.Body>
                         </Card>
-                        <Form>
+                        <Form onSubmit={handlePay}>
                             <Form.Group as={Row} className="mb-5">
                                 <Form.Label column sm={2} size="lg">Email</Form.Label>
                                 <Col sm={10}>
@@ -107,14 +164,14 @@ const Home = () => {
                             <Form.Group as={Row} className="mb-5" controlId="formBasicPassword">
                                 <Form.Label column sm={2}>Desde:</Form.Label>
                                 <Col sm={4}>
-                                    <Form.Control type="Date" />
+                                    <Form.Control type="Date" onChange={(event) => handleDates("from", event)} />
                                 </Col>
                                 <Form.Label column sm={2}>Hasta:</Form.Label>
                                 <Col sm={4}>
-                                    <Form.Control type="Date" />
+                                    <Form.Control type="Date" onChange={(event) => handleDates("to", event)} />
                                 </Col>
                             </Form.Group>
-                            <h5 className='total-rental'>TOTAL: $000.000</h5>
+                            <h5 className='total-rental'>TOTAL: ${price.toLocaleString(navigator.language, { minimumFractionDigits: 0, })}</h5>
 
                             <div className="rent-button">
                                 <Button type="submit">
@@ -123,6 +180,7 @@ const Home = () => {
                             </div>
 
                         </Form>
+                        <Pay payModal={payModal} setPayModal={setPayModal}/>
                     </div>
 
                 </section>
